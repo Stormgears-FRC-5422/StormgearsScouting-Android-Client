be4a62d9df1a28300bf96173c7097d3380f8a996
Michael Pavkovic <mpavkovic688@gmail.com>
2017-01-24 21:13:44 -0500

    Removed real phone number

--- app/src/main/java/org/stormgears/stormgearsscoutingv20/Constants.java
+++ app/src/main/java/org/stormgears/stormgearsscoutingv20/Constants.java
@@ -7,7 +7,7 @@ package org.stormgears.stormgearsscoutingv20;
 public class Constants
 {
 	public static final String MATCH_SCOUTING_FORM_KEY = "org.stormgears.stormgearsscoutingv20.match_form_contents";
-	public static final String PHONE_NUMBER = "***REMOVED***";
+	public static final String PHONE_NUMBER = "XXXXXXXXXX";
 	public static final int SMS_CHAR_LIMIT = 160;
 	public static final String MSG_PREFIX = "STORMGEARS_SCOUT";
 }
